c7607be93f51c83441218e451c4b2e30aa021f22
Terria-K <teuria@proton.me>
2025-03-14 00:25:57 +0800

    Updated TowerPatcher

--- OopsAllArrowsMod/TowerPatcher.cs
+++ OopsAllArrowsMod/TowerPatcher.cs
@@ -2,6 +2,7 @@
 
 namespace OopsAllArrowsMod
 {
+    [TowerPatcher("Darkfang")]
     public class Darkfang : TowerPatch
     {
          public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -9,6 +10,8 @@ namespace OopsAllArrowsMod
              Patcher.IncreaseTreasureRates(ModRegisters.PickupType<IceArrowPickup>());
          }
     }
+
+    [TowerPatcher("Thornwood")]
     public class Thornwood : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -16,6 +19,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<SlimeArrowPickup>());
         }
     }
+
+    [TowerPatcher("Moonstone")]
     public class Moonstone : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -23,6 +28,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<BaitArrowPickup>());
         }
     }
+
+    [TowerPatcher("Ascension")]
     public class Ascension : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -30,6 +37,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<PrismTrapArrowPickup>());
         }
     }
+
+    [TowerPatcher("Towerforge")]
     public class Towerforge : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -37,6 +46,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<LandMineArrowPickup>());
         }
     }
+
+    [TowerPatcher("King's Court")]
     public class KingsCourt : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -44,6 +55,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<MissleArrowPickup>());
         }
     }
+
+    [TowerPatcher("Cataclysm")]
     public class Cataclysm : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -51,6 +64,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<FreakyArrowPickup>());
         }
     }
+
+    [TowerPatcher("Flight")]
     public class Flight : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -58,6 +73,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<TornadoArrowPickup>());
         }
     }
+
+    [TowerPatcher("Backfire")]
     public class Backfire : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -65,6 +82,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<MechArrowPickup>());
         }
     }
+
+    [TowerPatcher("Dreadwood")]
     public class Dreadwood : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
@@ -72,6 +91,8 @@ namespace OopsAllArrowsMod
             Patcher.IncreaseTreasureRates(ModRegisters.PickupType<BoomerangArrowPickup>());
         }
     }
+
+    [TowerPatcher("Twilight Spire")]
     public class TwilightSpire : TowerPatch
     {
         public override void VersusPatch(VersusTowerPatchContext Patcher)
